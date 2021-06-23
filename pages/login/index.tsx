1dc520b6a7f22e83181d6f5c5299704a7dd11a35
mars <nappeodinsite@gmail.com>
2021-06-23 17:00:00 -0300

    feat: 404 page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import styles from "./styles.module.scss";
+import styles from "./styles/styles.module.scss";
 import Link from 'next/link'
 
 export default function Login() {
